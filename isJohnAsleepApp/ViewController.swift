b5b1d2a61153f3ab81cd182a4ce0137c3505561e
John Mars <JOHN@M4R5.io>
2015-08-22 23:56:15 -0400

    Create initial application
    Application is mostly working, with basic debug styling.

--- isJohnAsleepApp/ViewController.swift
+++ isJohnAsleepApp/ViewController.swift
@@ -9,17 +9,105 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	let asleepText = "I'm going to sleep"
+	let awakeText  = "I woke up"
+	
+	var asleep = false
+	
+	//	let baseUrlString = "http://www.isjohnasleep.com/"
+	let baseUrlString = "http://192.168.1.100:3000/"
 
+	
+	@IBOutlet weak var button: UIButton!
+	@IBAction func buttonPress(sender: AnyObject) {
+		
+		var request = HTTPTask()
+		
+		if asleep {
+			request.POST(baseUrlString + "awake", parameters: nil, completionHandler: {(response: HTTPResponse) in
+				
+				if let err = response.error {
+					println("error: \(err.localizedDescription)")
+					return
+				}
+				
+				if let data = response.responseObject as? NSData {
+					let str = NSString(data: data, encoding: NSUTF8StringEncoding)
+					println(str!)
+					
+					dispatch_async(dispatch_get_main_queue(), {
+						self.button.setTitle(self.asleepText, forState: .Normal)
+						UIView.animateWithDuration(1, animations: {
+							self.view.backgroundColor = UIColor.redColor()
+						})
+					})
+
+					self.asleep = !self.asleep
+				}
+			})
+		} else {
+			request.POST(baseUrlString + "asleep", parameters: nil, completionHandler: {(response: HTTPResponse) in
+				
+				if let err = response.error {
+					println("error: \(err.localizedDescription)")
+					return
+				}
+				
+				if let data = response.responseObject as? NSData {
+					let str = NSString(data: data, encoding: NSUTF8StringEncoding)
+					println(str!)
+					
+					dispatch_async(dispatch_get_main_queue(), {
+						self.button.setTitle(self.awakeText, forState: .Normal)
+						UIView.animateWithDuration(1, animations: {
+							self.view.backgroundColor = UIColor.blueColor()
+						})
+					})
+
+					self.asleep = !self.asleep
+				}
+			})
+		}
+		
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
+		
+		println(button.state)
+		
+		var request = HTTPTask()
+		request.GET(baseUrlString + "status", parameters: nil, completionHandler: {(response: HTTPResponse) in
+			
+			if let err = response.error {
+				println("error: \(err.localizedDescription)")
+				return
+			}
+			
+			if let data = response.responseObject as? NSData {
+				let str = NSString(data: data, encoding: NSUTF8StringEncoding)
+				
+				println(str)
+				
+				if str! == "asleep" {
+					dispatch_async(dispatch_get_main_queue(), {
+						self.button.setTitle(self.awakeText, forState: .Normal)
+					})
+					
+					self.asleep = true
+				} else {
+					dispatch_async(dispatch_get_main_queue(), {
+						self.button.setTitle(self.asleepText, forState: .Normal)
+					})
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+					self.asleep = false
+				}
+				
+			}
+		})
+		
 	}
-
 
 }
 
